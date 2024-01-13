5d04824f21653b0032c827d00c2f8a6823c90f51
LavenderPeach <134173296+LavenderPeach@users.noreply.github.com>
2024-01-13 14:41:27 -0500

    created a for loop that cycled through movies within my sql table and added the necessary information for them. used try/except block so script did not crash upon encountering an error in retrieving info.

--- omdb.py
+++ omdb.py
@@ -14,3 +14,31 @@ cursor = conn.cursor()
 
 cursor.execute("SELECT id, title FROM movie ORDER BY id desc")
 movies = cursor.fetchall()
+
+for movie in movies:
+    title = movie[1]
+    omdb_api_url = f"http://www.omdbapi.com/?t={title}&apikey={api_key}"
+    try:
+        response = requests.get(omdb_api_url)
+        response.raise_for_status()
+        data = response.json()
+
+        imdb_rating = data.get("imdbRating", "0")
+        mpaa_rating = data.get("Rated", "Unknown")
+        metascore = data.get("Metascore", "0")
+        imdb_count = data.get("imdbVotes", 0)
+        awards = data.get("Awards", "None")
+        poster = data.get("Poster", "No Poster")
+        box_office = data.get("BoxOffice", "0")
+        
+        update_query = "UPDATE movie SET imdb_score = %s, rating = %s, metascore = %s, rotten_tomatoes_score = %s, imdb_vote_count = %s, awards = %s, box_office = %s, poster_url = %s WHERE id = %s"
+        values = (imdb_rating, mpaa_rating, metascore, imdb_count, awards, box_office, poster, movie[0])
+
+        cursor.execute(update_query, values)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for {title}: {e}")
+
+conn.commit() 
+cursor.close()
+conn.close()
