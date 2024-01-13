ccee7ac20978d7edf3a697f4970972947105c775
LavenderPeach <134173296+LavenderPeach@users.noreply.github.com>
2024-01-13 14:50:49 -0500

    added if/else statements to many of the pull requests so that the data type of the information I received would match those in my sql table.
    for some pulls, such as rotten tomatoes, had to add '.replace' to remove any commas or extra symbols that would interfere with the 'int' datatype.

--- omdb.py
+++ omdb.py
@@ -24,16 +24,32 @@ for movie in movies:
         data = response.json()
 
         imdb_rating = data.get("imdbRating", "0")
+        if imdb_rating == 'N/A':
+            imdb_rating = '0'
         mpaa_rating = data.get("Rated", "Unknown")
         metascore = data.get("Metascore", "0")
+        if metascore == 'N/A':
+            metascore = '0'
         rotten_tomatoes = next((rating['Value'] for rating in data.get('Ratings', []) if 'Rotten Tomatoes' in rating.get('Source', '')), "N/A")
+        if rotten_tomatoes == 'N/A':
+            rotten_tomatoes_score = '0'
+        elif rotten_tomatoes != 'N/A':
+            rotten_tomatoes_score = int(rotten_tomatoes[:-1])
         imdb_count = data.get("imdbVotes", 0)
+        if imdb_count == 'N/A':
+            imdb_count_int = '0'
+        elif imdb_count != 'N/A':
+            imdb_count_int = int(imdb_count.replace(',', ''))
         awards = data.get("Awards", "None")
         poster = data.get("Poster", "No Poster")
         box_office = data.get("BoxOffice", "0")
+        if box_office == 'N/A':
+            box_office = '0'
+        elif box_office != 'N/A':
+            box_office = int(box_office.replace('$', '').replace(',', ''))
         
         update_query = "UPDATE movie SET imdb_score = %s, rating = %s, metascore = %s, rotten_tomatoes_score = %s, imdb_vote_count = %s, awards = %s, box_office = %s, poster_url = %s WHERE id = %s"
-        values = (imdb_rating, mpaa_rating, metascore, imdb_count, awards, box_office, poster, movie[0])
+        values = (imdb_rating, mpaa_rating, metascore, rotten_tomatoes_score, imdb_count_int, awards, box_office, poster, movie[0])
 
         cursor.execute(update_query, values)
 
